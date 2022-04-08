4941f399dc793106cff69d8ac74fc904b90363cc
techarm <techarmdev@gmail.com>
2022-04-09 00:02:52 +0900

    [app] ユーザー一覧表示処理を追加

--- vue-app/src/components/user/UserList.vue
+++ vue-app/src/components/user/UserList.vue
@@ -3,6 +3,22 @@
         <div class="row">
             <div class="col">
                 <h1 class="mt-3">User List</h1>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>User ID</th>
+                            <th>User Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="user in users" v-bind:key="user.id">
+                            <td>{{ user.id }}</td>
+                            <td>{{ user.first_name + ', ' + user.last_name }}</td>
+                            <td>{{ user.email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
