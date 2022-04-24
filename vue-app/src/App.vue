1f7332e4c99263ef38f4b21f45a21fcbd4ce01e2
techarm <techarmdev@gmail.com>
2022-04-24 12:19:19 +0900

    画面レイアウト微修正

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-view v-slot="{Component}" :key="componentKey" @success="success" @error="error" @warning="warning" @forceUpdate="forceUpdate">
-      <keep-alive include="['BookList']">
+      <keep-alive include="BookList">
         <component :is="Component" />
       </keep-alive>
     </router-view>
--- vue-app/src/components/admin/SiderMenu.vue
+++ vue-app/src/components/admin/SiderMenu.vue
@@ -2,9 +2,9 @@
     <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark" id="sidenav-main">
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-            <router-link class="navbar-brand m-0" to="/admin" target="_blank">
+            <router-link class="navbar-brand m-0" to="/" target="_blank">
                 <img src="/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold text-white">Teacharm Dashboard</span>
+                <span class="ms-1 font-weight-bold text-white">Teacharm Home</span>
             </router-link>
         </div>
         <hr class="horizontal light mt-0 mb-2">
